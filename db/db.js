16e4e1d9bc396acf7f9d58e04ac68ef4b094958b
Pravachan1203 <147231326+Pravachan1203@users.noreply.github.com>
2025-02-25 10:02:47 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,15 +2,17 @@ import mysql from 'mysql2/promise';
 
 async function createConnection() {
     try {
-        const connection = await mysql.createConnection({
-            host:'srv1752.hstgr.io',
-            user: 'u607585719_class',
-            password:'Classhos@79',
-            database:'u607585719_class',
-            waitForConnections: true,
-            connectionLimit: 100,  
-            queueLimit: 0
-        });
+       const connection = await mysql.createConnection({
+    host:'srv1752.hstgr.io',
+    user: 'u607585719_class',
+    password:'Classhos@79',
+    database:'u607585719_class',
+    waitForConnections: true,
+    connectionLimit: 10,  
+    queueLimit: 0,
+    connectTimeout: 10000  // 10 seconds
+});
+
         console.log('Database connected successfully');
         return connection;
     } catch (error) {
